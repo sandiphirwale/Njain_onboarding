d9fc01b6658590ec8b73da3b2a47f5f877ca998d
nikhil <nikhiljain00019@gmail.com>
2020-05-22 10:34:33 +0530

    adding review changes

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ function study1(){
     document.getElementById("card3").style.display = "none";
     
     for( let i=0; i<lenc; i++){
+        study[0].country[i].tagHospitals = [];
          document.getElementById("boxdrop1").innerHTML += 
-              study[0].country[i].name + " <br> " + " <div ondragover=allowDrop(event) ondrop=drop(event) id= "+ "box" + ">" +"</div";
+              study[0].country[i].name + " <br> " + " <div ondragover=allowDrop(event) ondrop=drop(event) class=box " + "id="+ study[0].country[i].name + ">" +"</div>";
     }
 
     for( let j=0; j<lenh; j++){
-        document.getElementById("hospdrag1").innerHTML += " <div draggable=true ondragstart=dragStart(event) class=dragitem id=draga"+ j +">"  + study[1].hospital[j] + "</div>" + " <br> " ;       
+        document.getElementById("hospdrag1").innerHTML += " <div draggable=true ondragstart=dragStart(event) ondragover=stop(event) class=dragitem id=draga"+ j +">"  + study[1].hospital[j] + "</div>" + " <br> " ;       
     }
 
 }
@@ -41,12 +42,13 @@ function study2(){
     document.getElementById("card3").style.display = "none";
 
     for( let i=0; i<lenc; i++){
+        study[0].country[i].tagHospitals = [];
         document.getElementById("boxdrop2").innerHTML +=
-             study[0].country[i].name + " <br> " + " <div ondragover=allowDrop(event) ondrop=drop(event) id= "+ "box" + ">" +"</div";
+             study[0].country[i].name + " <br> " + " <div ondragover=allowDrop(event) ondrop=drop(event) class=box " + "id="+ study[0].country[i].name + ">" +"</div>";
     }
 
     for( let j=0; j<lenh; j++){
-        document.getElementById("hospdrag2").innerHTML += " <div draggable=true ondragstart=dragStart(event) class=dragitem id=dragb"+ j +">"+ study[1].hospital[j] + "</div>" + " <br> ";
+        document.getElementById("hospdrag2").innerHTML += " <div draggable=true ondragstart=dragStart(event) ondragover=stop(event) class=dragitem id=dragb"+ j +">"+ study[1].hospital[j] + "</div>" + " <br> ";
     }
 }
 
@@ -58,25 +60,49 @@ function study3(){
     document.getElementById("card3").style.display = "block";
 
     for( let i=0; i<lenc; i++){
+        study[0].country[i].tagHospitals = [];
         document.getElementById("boxdrop3").innerHTML += 
-            study[0].country[i].name + " <br> " + " <div ondragover=allowDrop(event) ondrop=drop(event) id= "+ "box" + ">" +"</div";
+            study[0].country[i].name + " <br> " + " <div ondragover=allowDrop(event) ondrop=drop(event) class=box " + "id="+ study[0].country[i].name + ">" +"</div>";
     }
 
     for( let j=0; j<lenh; j++){
-        document.getElementById("hospdrag3").innerHTML += " <div draggable=true ondragstart=dragStart(event) class=dragitem id=dragc"+ j +">"+ study[1].hospital[j] + "</div>" + " <br> ";
+        document.getElementById("hospdrag3").innerHTML += " <div draggable=true ondragstart=dragStart(event) ondragover=stop(event) class=dragitem id=dragc"+ j +">"+ study[1].hospital[j] + "</div>" + " <br> ";
     }
 }
 
 function dragStart(ev){
-    droppableText = ev.target.id;
+   droppableText = ev;
+   for (let k=0; k<lenc; k++){
+      if(study[0].country[k].name == droppableText.target.parentElement.id ){
+          var index = study[0].country[k].tagHospitals.indexOf(droppableText.target.textContent);
+          study[0].country[k].tagHospitals.splice(index,1);
+      }
+   }
 }
 
 function allowDrop(ev){
     ev.preventDefault();
 }
 
+function stop(ev){
+    ev.stopPropagation();
+}
+
 function drop(ev){
-    ev.target.append(document.getElementById(droppableText));
+    ev.target.append(document.getElementById(droppableText.target.id));
+    document.getElementById(droppableText.target.id).style.margin = "10px";
+    for (let k=0; k<lenc; k++){
+        if(ev.target.id ==  study[0].country[k].name){
+            if(study[0].country[k].tagHospitals.length == 0){
+                study[0].country[k].tagHospitals.push(droppableText.target.textContent);
+            } else {
+                if ( study[0].country[k].tagHospitals.indexOf(droppableText.target.textContent) == -1){
+                   study[0].country[k].tagHospitals.push(droppableText.target.textContent);
+                }
+            }  
+            console.log("Updated hospital list in country",study);   
+        }        
+    }
 }
 
 
